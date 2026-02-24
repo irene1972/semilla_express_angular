797e9bce0e454043eb3d6a0bca374985372d9e4b
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-02-24 10:26:26 +0100

    ejemplo de pasar mensaje en un navigation

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './components/home/home';
 import { Contacto } from './components/contacto/contacto';
 import { C404 } from './components/c404/c404';
 import { Registro } from './components/registro/registro';
+import { Login } from './components/login/login';
 
 export const routes: Routes = [
     {path:'',pathMatch:'full',redirectTo:'home'},
     {path:'home',component:Home},
     {path:'contacto',component:Contacto},
     {path:'registro',component:Registro},
+    {path:'login',component:Login},
     {path:'**',component:C404}
 ];
